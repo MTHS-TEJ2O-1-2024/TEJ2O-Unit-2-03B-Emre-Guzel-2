dcd582285e6487bb0461c05f7160c1e2a888ce4e
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-09-17 12:05:05 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Emre Guzel
- * Created on: Sep 17
- * This program say Emre Guzel 
+ * Created on: Sep 17 2024
+ * This program claulates primeter and area of rectangle
+ * Perimeter=26cm
+area=15
 */
 
-basic.showString('Emre Guzel')
+basic.clearScreen()
+basic.pause(1000)
+basic.showString('2*(w+l)='+(2*(6.5+6.5 )+"cm"))
+basic.showString('A=L*W'+(2+5) )
