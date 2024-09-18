a81543f6a33387c9f62bcdbd69986b8dd83d3e82
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-09-18 22:24:30 +0000

    claculaeted and trying fix the problem of formot

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,14 +7,16 @@ Area 15 cm^2
 """
 
 from microbit import *
-from time import sleep
 
-display.clear
+from time import sleep
+display.clear()
+
 
 sleep(1)
 
-#display.scroll (2 (" l + w") + (4 * 4) + "cm")
 
-l=4
-w=4
-display.scroll(f"2{l}+{w}+{l* w}cm")
+l = 2
+w = 4
+perimeter = 2 * (l + w)
+
+display.scroll("Perimeter: " + str(perimeter) + "cm")
