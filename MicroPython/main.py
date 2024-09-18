4672460ccf9bb92962b74a3d9f2204d5e52fdb5f
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-09-18 01:17:51 +0000

    aded python vertoin

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,17 @@
+
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Emre Guzel
+Created on: Sep 17 2024
+This module is a Micro:bit and it can calculate the area and perimeter
 """
 
 from microbit import *
+from time import sleep
 
+display.clear
+
+sleep(1)
+
+display.scroll(1+2)
 
 display.scroll("Hello, World!")
