4f2c06704dfa8ebeab450bf537f4dff56df4926a
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-09-18 22:49:55 +0000

    researting the rohject

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,22 +1,19 @@
 """
 Created by: Emre Guzel
 Created on: Sep 17 2024
-This module is a Micro:bit and it can calculate the area and perimeter
-Perimeter 16 cm
-Area 15 cm^2
+This module is for a Micro:bit and can calculate the area and perimeter.
+Perimeter: 16 cm
+Area: 15 cm^2
 """
 
 from microbit import *
-
 from time import sleep
+
+
 display.clear()
 
 
-sleep(1)
+sleep(1)  
 
 
-l = 2
-w = 4
-perimeter = 2 * (l + w)
-
-display.scroll("Perimeter: " + str(perimeter) + "cm")
+display.scroll(str(3 + 2))
