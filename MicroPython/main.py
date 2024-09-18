b919ed7eadc187279ec60e1f2f22dd6ab62d0d22
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-09-18 16:05:58 +0000

    updated the format

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -9,11 +9,12 @@ Area 15 cm^2
 from microbit import *
 from time import sleep
 
-
 display.clear
 
 sleep(1)
 
+#display.scroll (2 (" l + w") + (4 * 4) + "cm")
 
-display.scroll ("2(l+w)" + (4*4) + 'cm')
-
+l=4
+w=4
+display.scroll(f"2{l}+{w}+{l* w}cm")
